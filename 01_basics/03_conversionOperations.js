fe11ed0047c3f2c91f4b2d0676115c5da4d86229
Vivek Kumar Kandu <vivekk11951@gmail.com>
2026-06-06 01:07:15 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,37 @@
+//let score = 33
+//let score = "33"
+//let score = "33abc"
+//let score = null
+//let score = undefined
+//let score = true
+let score = "vivek";
+
+/*  "33"=> 33
+    "33abc"=> NaN(not a number ) but iska type number hi hai 
+    true =>1,false=> 0
+*/
+
+//console.log(score);
+console.log(typeof score); //two ype for checking their type
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// let isloggedin = 1
+//let isloggedin = ""
+let isloggedin = "vivek";
+let booleanIsLoggedin = Boolean(isloggedin);
+console.log(booleanIsLoggedin);
+
+// 1 => true , 0=> false
+//"" => false
+// " vivek" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
+console.log(stringNumber);
