226cbbc9e4e7fc708feefc5f970785fc71d3e210
Vivek Kumar Kandu <vivekk11951@gmail.com>
2026-06-06 07:42:16 +0000

    operation and comparision

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,37 +1,62 @@
-//let score = 33
-//let score = "33"
-//let score = "33abc"
-//let score = null
-//let score = undefined
-//let score = true
-let score = "vivek";
+// //let score = 33
+// //let score = "33"
+// //let score = "33abc"
+// //let score = null
+// //let score = undefined
+// //let score = true
+// let score = "vivek";
 
-/*  "33"=> 33
-    "33abc"=> NaN(not a number ) but iska type number hi hai 
-    true =>1,false=> 0
-*/
+// /*  "33"=> 33
+//     "33abc"=> NaN(not a number ) but iska type number hi hai
+//     true =>1,false=> 0
+// */
 
-//console.log(score);
-console.log(typeof score); //two ype for checking their type
-console.log(typeof score);
+// //console.log(score);
+// console.log(typeof score); //two ype for checking their type
+// console.log(typeof score);
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-// let isloggedin = 1
-//let isloggedin = ""
-let isloggedin = "vivek";
-let booleanIsLoggedin = Boolean(isloggedin);
-console.log(booleanIsLoggedin);
+// // let isloggedin = 1
+// //let isloggedin = ""
+// let isloggedin = "vivek";
+// let booleanIsLoggedin = Boolean(isloggedin);
+// console.log(booleanIsLoggedin);
 
-// 1 => true , 0=> false
-//"" => false
-// " vivek" => true
+// // 1 => true , 0=> false
+// //"" => false
+// // " vivek" => true
 
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(stringNumber);
 
-console.log(stringNumber);
+// // ************  operation **********
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+// let str1 = "helllo";
+// let str2 = " vivek";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log("1" + (2 + 2));
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,32 @@
+// console.log( 2 > 3 );
+// console.log( 2 >= 3 );
+// console.log( 2 < 3 );
+// console.log( 2 >= 3 );
+// console.log( 2 != 3 );
+// console.log( 2 == 3 );
+
+
+// console.log( "2" > 1 );
+// console.log( "02" > 1 );
+
+ /* // iss tarah ke conversion avoid karna hai 
+// console.log( null > 0 );
+// console.log( null >= 0 );
+// console.log( null == 0 );
+// console.log( null < 0 );
+// console.log( null <= 0 );
+
+// console.log( undefined > 0 );
+// console.log( undefined >= 0 );
+// console.log( undefined == 0 );
+// console.log( undefined < 0 );
+// console.log( undefined <= 0 );
+
+*/
+
+// strictly check (===)  means values ko strictly check karta hai means datatypes ko bhi check krta hai
+
+console.log( "2" === 2);
+
+
+
